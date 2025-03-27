e23ddc02d61c207ee1717fd5c8141f13bf825f1a
Ciro Salaberry <cirosalaberry@gmail.com>
2025-03-27 13:52:42 -0300

    ejercicio3

--- main.js
+++ main.js
@@ -34,4 +34,47 @@ function borrarInputs() {
     }
 }
 
+/*
+let matrizAleatoria = [];
+for (let i = 0; i < 100; i++) {
+  matrizAleatoria[i] = [];
+  for (let j = 0; j < 100; j++) {
+    matrizAleatoria[i][j] = Math.floor(Math.random() * 100) + 1;
+  }
+} */
+
+function valorMatriz(matriz){
+    let fila = 0
+    let resultado = 0
+    while(fila<matriz.length){
+        let columna = 0
+        while(columna<matriz[fila].length){
+            resultado += matriz[fila][columna]
+            columna++
+        }
+        fila++
+    }
+    return resultado
+}
+
+function promedioMatriz(matriz){
+    let arregloPromedios = [];
+    let promedioFila = 0;
+    let fila = 0
+    let resultado = 0
+    while(fila<matriz.length){
+        let columna = 0
+        while(columna<matriz[fila].length){
+            resultado += matriz[fila][columna]
+            columna++
+        }
+        promedioFila = resultado/matriz[fila].length;
+        arregloPromedios.push(promedioFila)
+        fila++
+        resultado = 0
+    }
+    return arregloPromedios
+}
+
+
 
