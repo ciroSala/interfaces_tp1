71a26ecfaa273be65875b92e5a202a4db4607d48
Ciro Salaberry <cirosalaberry@gmail.com>
2025-04-01 15:35:34 -0300

    ejercicio4

--- main.js
+++ main.js
@@ -1,80 +1,95 @@
-const body = document.getElementById("body");
-const botonColor = document.getElementById("boton");
-const boton2 = document.getElementById("boton2");
-let inputs = document.getElementsByClassName("input");
+document.addEventListener("DOMContentLoaded", function() {
+    const BODY = document.getElementById("body");
+    const BOTONCOLOR = document.getElementById("botonColor");
+    const BOTON = document.getElementById("botonInputs");
+    let inputs = document.getElementsByClassName("input");
+    
+    /**
+    * @type {HTMLCanvasElement}
+    */
+    const CANVAS = document.getElementById("canvas");
+    
+    /**
+    * @type {CanvasRenderingContext2D}
+    */
+    let ctx = CANVAS.getContext("2d");
+    ctx.beginPath();
+    ctx.fillStyle = 'green';
+    ctx.fillRect(0, 0, 300, 100);
 
-botonColor.addEventListener("click", cambiarFondo);
-boton2.addEventListener("click", borrarInputs);
+    BOTONCOLOR.addEventListener("click", cambiarFondo);
+    BOTON.addEventListener("click", borrarInputs);
 
 
-let colores = {
-    1: ["red","pink"],
-    2: ["black", "brown"],
-    3: ["blue","aquamarine"],
-    4: ["grey","white"],
-    5: ["green","greenyellow"]
-};
+    let colores = {
+        1: ["red","pink"],
+        2: ["black", "brown"],
+        3: ["blue","aquamarine"],
+        4: ["grey","white"],
+        5: ["green","greenyellow"]
+    };
 
-let numeroViejo = 0;
-let numero = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+    let numeroViejo = 0;
+    let numero = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
 
-function cambiarFondo() {
-    numero = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
-    while (numero==numeroViejo) 
+    function cambiarFondo() {
         numero = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
-    numeroViejo = numero;
-    body.style.backgroundColor = colores[numero][0];
-    botonColor.style.backgroundColor = colores[numero][1];
-}
-
-function borrarInputs() {
-    for(let i = 0; i < inputs.length; i++){
-        console.log(inputs[i]);
-        inputs[i].value = "";
+        while (numero==numeroViejo) 
+            numero = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+        numeroViejo = numero;
+        BODY.style.backgroundColor = colores[numero][0];
+        BOTONCOLOR.style.backgroundColor = colores[numero][1];
     }
-}
 
-/*
-let matrizAleatoria = [];
-for (let i = 0; i < 100; i++) {
-  matrizAleatoria[i] = [];
-  for (let j = 0; j < 100; j++) {
-    matrizAleatoria[i][j] = Math.floor(Math.random() * 100) + 1;
-  }
-} */
-
-function valorMatriz(matriz){
-    let fila = 0
-    let resultado = 0
-    while(fila<matriz.length){
-        let columna = 0
-        while(columna<matriz[fila].length){
-            resultado += matriz[fila][columna]
-            columna++
+    function borrarInputs() {
+        for(let i = 0; i < inputs.length; i++){
+            console.log(inputs[i]);
+            inputs[i].value = "";
         }
-        fila++
     }
-    return resultado
-}
 
-function promedioMatriz(matriz){
-    let arregloPromedios = [];
-    let promedioFila = 0;
-    let fila = 0
-    let resultado = 0
-    while(fila<matriz.length){
-        let columna = 0
-        while(columna<matriz[fila].length){
-            resultado += matriz[fila][columna]
-            columna++
-        }
-        promedioFila = resultado/matriz[fila].length;
-        arregloPromedios.push(promedioFila)
-        fila++
-        resultado = 0
+    /*
+    let matrizAleatoria = [];
+    for (let i = 0; i < 100; i++) {
+    matrizAleatoria[i] = [];
+    for (let j = 0; j < 100; j++) {
+        matrizAleatoria[i][j] = Math.floor(Math.random() * 100) + 1;
     }
-    return arregloPromedios
-}
+    } */
+
+    function valorMatriz(matriz){
+        let fila = 0
+        let resultado = 0
+        while(fila<matriz.length){
+            let columna = 0
+            while(columna<matriz[fila].length){
+                resultado += matriz[fila][columna]
+                columna++
+            }
+            fila++
+        }
+        return resultado
+    }
+
+    function promedioMatriz(matriz){
+        let arregloPromedios = [];
+        let promedioFila = 0;
+        let fila = 0
+        let resultado = 0
+        while(fila<matriz.length){
+            let columna = 0
+            while(columna<matriz[fila].length){
+                resultado += matriz[fila][columna]
+                columna++
+            }
+            promedioFila = resultado/matriz[fila].length;
+            arregloPromedios.push(promedioFila)
+            fila++
+            resultado = 0
+        }
+        return arregloPromedios
+        }
+})
 
 
 
